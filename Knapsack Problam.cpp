2a3419d76f192eceef7c0c4ec4b9cf56a4eee40b
Md. Motiur Rahman <motiur0883@gmail.com>
2026-03-24 22:12:23 +0600

    Implement Knapsack Problem solution in C++

---
+++ Knapsack Problam.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,W;
+    cin>>n>>W;
+
+    int weight[100],value[100];
+
+    for(int i=1;i<=n;i++)
+        cin>>weight[i];
+
+    for(int i=1;i<=n;i++)
+        cin>>value[i];
+
+    int dp[100][100];
+
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=W;j++)
+        {
+            if(i==0 || j==0)
+                dp[i][j]=0;
+
+            else if(weight[i] <= j)
+                dp[i][j] = max(value[i] + dp[i-1][j-weight[i]], dp[i-1][j]);
+
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+
+    cout<<"Maximum Value = "<<dp[n][W];
+}
