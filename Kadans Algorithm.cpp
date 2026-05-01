4a388637142bef5d641175fcc288055752fd94c8
Md. Motiur Rahman <motiur0883@gmail.com>
2026-05-01 19:00:46 +0600

    Implement Kadane's Algorithm for maximum subarray sum

---
+++ Kadans Algorithm.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int currentSum = arr[0];
+    int maxSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        currentSum = max(arr[i], currentSum + arr[i]);
+        maxSum = max(maxSum, currentSum);
+    }
+
+    cout << "Maximum Subarray Sum = " << maxSum << endl;
+
+    return 0;
+}
